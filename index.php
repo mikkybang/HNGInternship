d8b9f526a4472739db1487501a82d34a8dc00e66
mikkybang <mefemena@gmail.com>
2018-04-10 17:58:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+
+</head>
+<body>
+    <header>
+        My Simple Page
+    </header>
+
+
+    <div class="time">
+            <span>Welcome to Michael Efemena’s simple page</span>
+    </br>
+    </br>
+            <span>The current time is <?php $today = date("H:i:s");
+                    echo $today;
+                    ?>
+            </span>
+            
+    </div>
+
+</body>   
+
+
+</html>
+    
